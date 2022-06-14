b0a4eb0745bb69ac7b21cdcfd8621ea03629164a
picem624 <PicEm624@school.lu>
2022-06-14 14:19:40 +0200

    deleted unnescessary code
    I deleted comments and a bunch of variable initialisations in the js file. I changed the structure of the folders a bit and changed the hTML rreference to those restructured files

--- js/pdfExport.js
+++ js/pdfExport.js
@@ -1,34 +1,6 @@
 
 function exportPDF(){
-
-    var title = document.getElementById("title").value;
-    var lFName = document.getElementById("lFName").innerText;
-    var fName = document.getElementById("fName").value;
-    var lLname = document.getElementById("lLName").innerText;
-    var lName = document.getElementById("lName").value;
-    var lAddress = document.getElementById("lAddress").innerText;
-    var address = document.getElementById("address").value;
-    var lLocation = document.getElementById("lLocation").innerText;  
-    var Location = document.getElementById("location").value;
-    
-
     var element = document.getElementById("data");
     html2pdf(element);
-    /*var doc = new jsPDF();
-    
-    doc.setFontSize(16);
-    doc.text(lFName + "  " + fName + "<br>" ,10 ,20);
-    doc.text(lLname + "  " + lName + "<br>" ,10 ,20);
-    doc.text(lAddress + "  " + address + "<br>",10 ,20);
-    doc.text(lLocation + "  " + Location + "<br>" ,10 ,20);
-    doc.fromHTML(document.getElementById("data"), 
-    15,
-    15, 
-    {'width': 170},
-    function() 
-     {
-      doc.save("PDF_Documet.pdf");
-    });
-  */
    
 }
