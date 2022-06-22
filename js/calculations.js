e750d888bb50b1bccde1d0fc6bfe6714ee6c186a
picem624 <PicEm624@school.lu>
2022-06-22 12:32:37 +0200

    working on calculations
    Now we are able to calculate the prices for the machines according to teh minutes used. Therefore I use a percentage calculation.
    I take the minutes written in the textfield and divide through 1 hour or rather 60mins. This makes it easier to calculate the price for the machine
    as the price for 1 machine over a timespan of 1 hour costs 5€. That means if the user inputs 60 in the minutes textfield. We have a percentage of 1%.
    then we simply multiply the (price for one machine per hour * percentage * quantity)
    
    for the workers we nearly use the same approach

--- js/calculations.js
+++ js/calculations.js
@@ -5,6 +5,9 @@ let timeTextboxes = document.getElementsByClassName("time");
 let MachineTxtBoxes = document.getElementsByClassName("Machine");
 let quantityTextboxes = document.getElementsByClassName("Quantity");
 let priceTextboxes = document.getElementsByClassName("Price");
+let machineTimeTextBoxes = document.getElementsByClassName("MachineTime");
+let workerNameBoxes = document.getElementsByClassName("WorkerName");
+let workerTimeBoxes = document.getElementsByClassName("WorkerTime");
 
 // Arrays
 var checkBoxarray =[];
@@ -12,12 +15,18 @@ var timeTxtArray = [];
 var MachineArray = [];
 var quantityArray = [];
 var priceArray = [];
+var machineTimeArray = [];
+var workerNameArray = [];
+var workerTimeArray = [];
 
 // Time variable
 var totalMins = 0;
 var totalHours = 0;
+var Mins = 0;
 
-
+// Tarif
+var workerPerHour = 16; 
+var machinePerHour = 5;
 // functions / Funktionen
 function setArrays(){
     
@@ -35,6 +44,15 @@ function setArrays(){
     }
     for(var i = 0; i < priceTextboxes.length; i++) {
         priceArray.push(priceTextboxes[i])
+    }
+    for(var i = 0; i < machineTimeTextBoxes.length; i++) {
+        machineTimeArray.push(machineTimeTextBoxes[i])
+    }
+    for(var i = 0; i < workerNameBoxes.length; i++) {
+        workerNameArray.push(workerNameBoxes[i])
+    }
+    for(var i = 0; i < workerTimeBoxes.length; i++) {
+        workerTimeArray.push(workerTimeBoxes[i])
     }
 }
 
@@ -55,6 +73,7 @@ function calculateTotalTime(){
     if (totalMins < 10) {
     
         document.getElementById("TTime").value = parseInt(totalHours) + ":" + "0" + parseInt(totalMins);
+        Mins = parseInt(totalMins)
     }
 
     else if (totalMins == 0) {
@@ -63,9 +82,10 @@ function calculateTotalTime(){
 
     else {
         document.getElementById("TTime").value = parseInt(totalHours) + ":" + parseInt(totalMins);
+        Mins = parseInt(totalMins)
     }
     totalMins = 0;
-    totalHours = 0
+    totalHours = 0;
 }
 
 
@@ -87,8 +107,8 @@ function addMachine(number){
                         break; 
                         }
                     }
-                    if(timeTxtArray[0].value != " ") {
-                        timeTxtArray[0].value = " ";
+                    if(timeTxtArray[0].value != "") {
+                        timeTxtArray[0].value = "";
                     }
                 }
             break;
@@ -108,8 +128,8 @@ function addMachine(number){
                         break;
                     }
                 }
-                if(timeTxtArray[1].value != " ") {
-                    timeTxtArray[1].value = " ";
+                if(timeTxtArray[1].value != "") {
+                    timeTxtArray[1].value = "";
                 }
             }
             break;
@@ -128,8 +148,8 @@ function addMachine(number){
                         break;
                     }
                 }
-                if(timeTxtArray[3].value != " ") {
-                    timeTxtArray[3].value = " ";
+                if(timeTxtArray[3].value != "") {
+                    timeTxtArray[3].value = "";
                 }
             }
             break;
@@ -165,32 +185,158 @@ function addMachine(number){
                             break;
                         }
                     }
-                    if(timeTxtArray[8].value != " ") {
-                        timeTxtArray[8].value = " ";
+                    if(timeTxtArray[8].value != "") {
+                        timeTxtArray[8].value = "";
                     }
                 }
                 break;
     }
 }
 
-function calculatePrice(number){
+function calculateMachinePrice(number){
     switch  (number) {
         case 1:
-            alert(quantityArray[0].value)
-        
-        /*if(MachineArray[0].value !="/" && quantityArray[0].value != " "|| quantityArray[0].value != 0 ){
-                priceArray[0].value = 2*10;
-
-            }*/
+        // we first check if every field is filled with a value, if not we do not print anything into the price box
+        if((MachineArray[0].value !="/" && quantityArray[0].value != "" && timeTxtArray[0] != "")
+            ||( MachineArray[0].value !="/" && quantityArray[0].value != 0  && timeTxtArray[0] != 0)){
+                    var time = machineTimeArray[0].value;
+                    var result = time / 60;
+                    var price = machinePerHour * result * quantityArray[0].value;
+                    price = Number(price.toFixed(2)) //  EN: limit to 2 digits  behind komma // DE: Limitiert auf 2 Stellen hinter dem Komma
+                    if(price < 10 ){
+                            priceArray[0].value = "0"+ price + "€"
+                    }
+                    else{
+                            priceArray[0].value = price + "€"
+                    }
+            }
+            else{
+                    priceArray[0].value = "";
+            }
             break;
         case 2:
+            if((MachineArray[1].value !="/" && quantityArray[1].value != "" && timeTxtArray[1] != "")
+            ||( MachineArray[1].value !="/" && quantityArray[1].value != 0  && timeTxtArray[1] != 0)){
+                    var time = machineTimeArray[0].value;
+                    var result = time / 60;
+                    var price = machinePerHour * result * quantityArray[0].value;
+                    price = Number(price.toFixed(2))
+                    if(price < 10 ){
+                        priceArray[1].value = "0"+ price + "€"
+                    }
+                    else{
+                        priceArray[1].value = price + "€"
+                    }
+                }
+            else{
+                    priceArray[1].value = "";
+            }
             break;
         case 3: 
+            if((MachineArray[2].value !="/" && quantityArray[2].value != "" && timeTxtArray[2] != "")
+            ||( MachineArray[2].value !="/" && quantityArray[2].value != 0  && timeTxtArray[2] != 0)){
+                    var time = machineTimeArray[0].value;
+                    var result = time / 60;
+                    var price = machinePerHour * result * quantityArray[0].value;
+                    price = Number(price.toFixed(2))
+                    if(price < 10 ){
+                            priceArray[2].value = "0"+ price + "€"
+                    }
+                    else{
+                            priceArray[2].value = price + "€"
+                    }
+            }
+            else{
+                    priceArray[2].value = "";
+            }
             break;
         case 4:
+            if((MachineArray[3].value !="/" && quantityArray[3].value != "" && timeTxtArray[3] != "")
+            ||( MachineArray[3].value !="/" && quantityArray[3].value != 0  && timeTxtArray[3] != 0)){
+                    var time = machineTimeArray[0].value;
+                    var result = time / 60;
+                    var price = machinePerHour * result * quantityArray[0].value;
+                    price = Number(price.toFixed(2))
+                    if(price < 10 ){
+                            priceArray[3].value = "0"+ price + "€"
+                    }
+                    else{
+                            priceArray[3].value = price + "€"
+                    }
+            }
+            else{
+                    priceArray[3].value = "";
+            }
             break;
-
-
     }
 
+}
+function calculateWorkerPrice(number){
+    switch (number) {
+      case 1:
+          if(workerNameArray[0].value != ""){
+            workerTimeArray[0].value = document.getElementById("TTime").value;
+            var hours = parseInt(document.getElementById("TTime").value);
+            var minutesPercentage = Mins / 60;
+            var result = hours * workerPerHour +(workerPerHour * minutesPercentage);
+            priceArray[4].value = Number(result.toFixed(2));
+        }
+        else{
+            priceArray[4].value = "";
+            workerTimeArray[0].value = "";
+        }
+        break; 
+      case 2:
+        if(workerNameArray[1].value != ""){
+            workerTimeArray[1].value = document.getElementById("TTime").value;
+            var hours = parseInt(document.getElementById("TTime").value);
+            var minutesPercentage = Mins / 60;
+            var result = hours * workerPerHour +(workerPerHour * minutesPercentage);
+            priceArray[5].value = Number(result.toFixed(2));
+        }
+        else{
+            priceArray[5].value = "";
+            workerTimeArray[1].value = "";
+        }
+          break;
+      case 3:
+        if(workerNameArray[2].value != ""){
+            workerTimeArray[2].value = document.getElementById("TTime").value;
+            var hours = parseInt(document.getElementById("TTime").value);
+            var minutesPercentage = Mins / 60;
+            var result = hours * workerPerHour +(workerPerHour * minutesPercentage);
+            priceArray[6].value = Number(result.toFixed(2));
+        }
+        else{
+            priceArray[6].value = "";
+            workerTimeArray[2].value = "";
+        }
+          break;
+      case 4:
+        if(workerNameArray[3].value != ""){
+            workerTimeArray[3].value = document.getElementById("TTime").value;
+            var hours = parseInt(document.getElementById("TTime").value);
+            var minutesPercentage = Mins / 60;
+            var result = hours * workerPerHour +(workerPerHour * minutesPercentage);
+            priceArray[7].value = Number(result.toFixed(2));
+        }
+        else{
+            priceArray[7].value = "";
+            workerTimeArray[3].value = "";
+        }
+          break;
+      case 5: 
+        if(workerNameArray[4].value != ""){
+        workerTimeArray[4].value = document.getElementById("TTime").value;
+        var hours = parseInt(document.getElementById("TTime").value);
+        var minutesPercentage = Mins / 60;
+        var result = hours * workerPerHour +(workerPerHour * minutesPercentage);
+        priceArray[8].value = Number(result.toFixed(2));
+        }
+        else{
+            priceArray[8].value = "";
+            workerTimeArray[4].value = "";
+        }
+        break;
+    }
 }
